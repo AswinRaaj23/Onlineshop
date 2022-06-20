3054367e0f116a4169eae8cd1d77e951b57aa3d8
Aswinraaj23 <aswinraaj26@gmail.com>
2022-06-20 17:58:45 +0530

    Prepare the project for internationalization by configuring settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -116,7 +117,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
+Languages = (
+    ('en', 'English'),
+    ('es', 'Spanish')
+)
 
 TIME_ZONE = 'UTC'
 
