c60d30e53b6925081e464cd3b552a28eb81946bb
Aswinraaj23 <aswinraaj26@gmail.com>
2022-06-20 13:00:33 +0530

    Send pdf files by email

--- payment/views.py
+++ payment/views.py
@@ -2,7 +2,7 @@ import braintree
 from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
 from orders.models import Order
-
+from .tasks import payment_completed
 # Create your views here.
 
 # instantiate Braintree payment gateway
@@ -25,6 +25,8 @@ def payment_process(request):
                     # store the unique transaction id
             order.braintree_id = result.transaction.id
             order.save()
+            # launch asynchronous task
+            payment_completed.delay(order.id)
             return redirect('payment:done')
         else:
             return redirect('payment:canceled')
